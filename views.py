29cfd59ed7a652b1524e61e15d689fdc17e8463b
Amit Upadhyay <github@amitu.com>
2009-03-14 05:41:46 +0530

    kind of working. only returning 25 entries no matter what i try

--- views.py
+++ views.py
@@ -5,6 +5,7 @@ import urllib
 import atom
 import gdata.contacts
 import gdata.contacts.service
+from gdata.auth import AuthSubToken
 # }}} 
 
 # start # {{{ 
@@ -17,6 +18,7 @@ def start(request):
     scope = 'http://www.google.com/m8/feeds/'
     secure = False
     session = True
+    gd_client = gdata.contacts.service.ContactsService()
     authSubLogin = gd_client.GenerateAuthSubURL(next, scope, secure, session)
 
     return HttpResponseRedirect(authSubLogin)
@@ -26,9 +28,13 @@ def start(request):
 def return_(request):
     next = request.GET["next"]
     gd_client = gdata.contacts.service.ContactsService()
-    gd_client.auth_token = request.GET["token"]
-    gd_client.UpgradeToSessionToken()
-    feed = gd_client.GetContactsFeed()
+    token = AuthSubToken()
+    token.set_token_string(request.GET["token"])
+    gd_client.UpgradeToSessionToken(token)
+
+    query = gdata.contacts.service.ContactsQuery()
+    query.max_result = 500 # still only returning 25
+    feed = gd_client.GetContactsFeed(query.ToUri())
 
     request.session["dgci_contact_feed"] = feed
     return HttpResponseRedirect(next)
